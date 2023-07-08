b7047459cd0dffa0aa3d845841fb33844543cd47
inesmresende <inesmresende@gmail.com>
2023-07-08 17:43:12 +0100

    acronym

--- src/main/java/upskill/eCafetaria/domain/Acronym.java
+++ src/main/java/upskill/eCafetaria/domain/Acronym.java
@@ -9,6 +9,9 @@ public class Acronym {
     private final String acronym;
 
     public Acronym(String acronym) {
+        if (acronym.isEmpty() || acronym == null || acronym.length() > 10 || acronym.trim().split(" ").length != 1 || !acronym.matches("[a-z A-Z]")) {
+            throw new IllegalArgumentException("Acrónimo inválido");
+        }
         this.acronym = acronym;
     }
 
