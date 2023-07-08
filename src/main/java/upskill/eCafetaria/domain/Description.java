622102d50671150ff27ab26dd42829de2b419610
Mariana Viana <marianaviana91@gmail.com>
2023-07-08 17:44:20 +0100

    description validations

--- src/main/java/upskill/eCafetaria/domain/Description.java
+++ src/main/java/upskill/eCafetaria/domain/Description.java
@@ -8,6 +8,8 @@ public class Description {
     private final String description;
 
     public Description(String description) {
+        if (description ==null || description.length() <1 || description.length() >99) { throw new IllegalArgumentException("Description must be between 1 and 100 characters");
+        }
         this.description = description;
     }
 
