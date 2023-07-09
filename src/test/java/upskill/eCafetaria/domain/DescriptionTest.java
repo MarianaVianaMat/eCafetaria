11e0d8389cebb8460005954f00c1f8c0a0250e1e
inesmresende <inesmresende@gmail.com>
2023-07-09 15:39:11 +0100

    toDescription update

---
+++ src/test/java/upskill/eCafetaria/domain/DescriptionTest.java
@@ -0,0 +1,27 @@
+package upskill.eCafetaria.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DescriptionTest {
+
+    @Test
+    void ensureDescriptionIsNotNullThorwException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new DishType(new Acronym("peixe"), new Description(null)));
+    }
+    @Test
+    void ensureAcronymLessThanHundredCharThrowException() {
+        StringBuilder string = new StringBuilder();
+        for (int i = 0; i <= 100; i++) {
+            string.append("hi");
+        }
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new DishType(new Acronym("carne"), new Description(string.toString())));
+    }
+
+    @Test
+    void ensureDescriptionIsNotEmptyThorwException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new DishType(new Acronym("peixe"), new Description("")));
+    }
+}
